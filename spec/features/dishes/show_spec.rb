58bd81bd91dcd3251a548c55229d43dd71b348cf
Chee Lee <cheeleertr@gmail.com>
2024-06-01 12:37:08 -0500

    add view for dish show

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -4,9 +4,11 @@ RSpec.describe "the dishes show page" do
 
   before :each do
     @chef1 = Chef.create!(name: "Chee")
-    @dish1 = @chef1.create!(name: "Grilled Cheese", description: "Bread and cheese, grilled")
+    @dish1 = @chef1.dishes.create!(name: "Grilled Cheese", description: "Bread and cheese, grilled")
     @ingredient1 = Ingredient.create!(name:"Bread", calories: 50)
     @ingredient2 = Ingredient.create!(name:"Cheese", calories: 100)
+    DishIngredient.create!(dish_id: @dish1.id, ingredient_id: @ingredient1.id)
+    DishIngredient.create!(dish_id: @dish1.id, ingredient_id: @ingredient2.id)
 
   end
 
@@ -18,17 +20,18 @@ RSpec.describe "the dishes show page" do
 # And I see the chef's name.
   describe "US 1" do
 
-    xit "When I visit a dish's show page
+    it "When I visit a dish's show page
     I see the dish’s name and description
     And I see a list of ingredients for that dish
     and a total calorie count for that dish
     And I see the chef's name." do
-      visit "/dishes/:id"
-
+      visit "/dishes/#{@dish1.id}"
+save_and_open_page
       expect(page).to have_content("Name: #{@dish1.name}")
       expect(page).to have_content("Description: #{@dish1.description}")
-      expect(page).to have_content("Ingredients: #{@ingredient1.name}", "#{@ingredient2.name}")
+      expect(page).to have_content("Ingredients: #{@ingredient1.name}, #{@ingredient2.name}")
       expect(page).to have_content("Calorie Count: #{@dish.calories}")
+      expect(page).to have_content("Chef: #{@dish.chef.name}")
     end
   end
 
