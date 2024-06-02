6fc756122f65e29239ff84960e58d4a171a4b36b
Clyde <clyde.autin@gmail.com>
2024-06-02 12:05:02 -0700

    test done for us 2

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'the show dishes page' do
+
   describe 'as a visitor' do
     it 'shows all details for a dish' do
       chefA = Chef.create!(name: 'Ice Man')
@@ -20,6 +21,26 @@ RSpec.describe 'the show dishes page' do
         expect(page).to have_content("Ingredient name: #{ingredient.name}")
         expect(page).to have_content("Calories: #{ingredient.calories}")
       end
+    end
+
+    it 'lets me add an ingredient to the dish' do
+      chefA = Chef.create!(name: 'Ice Man')
+      spag = chefA.dishes.create!(name: 'Spaghetti a la Marinara', description: 'Easy to make!')
+      pasta = Ingredient.create!(name: 'Spaghetti', calories: 200)
+      sauce = Ingredient.create!(name: 'Marinara', calories: 50)
+      DishIngredient.create!(dish: spag, ingredient: pasta)
+      DishIngredient.create!(dish: spag, ingredient: sauce)
+      parmesan = Ingredient.create!(name: 'Parmesan', calories:22 )
+
+      visit "/dishes/#{spag.id}"
+
+      expect(page).to have_button("Add Ingredient")
+      fill_in ingredient_id, with: parmesan.id
+      click_button("Add Ingredient")
+      expect(current_path).to eq("/dishes/#{spag.id}")
+      expect(page).to have_content("Ingredient name: #{parmesan.name}")
+      expect(page).to have_content("Calories: #{parmesan.calories}")
+
     end
   end
 end
