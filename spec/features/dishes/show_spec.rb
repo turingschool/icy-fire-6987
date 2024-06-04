7e5d73a4fdbb989ad8013b91e73f1770a0610ed8
Jan Lehigh <janlehigh461437@gmail.com>
2024-06-04 16:11:35 -0600

    Add user story 2 test.

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -24,8 +24,8 @@ RSpec.describe "Dish show page" do
 
       visit dish_path(dish_1)
 
+      expect(page).to have_content("Dish Ingredients")
       within('#dish_ingredient_list') do
-        expect(page).to have_content("Dish Ingredients")
         expect(page).to have_content("Chicken Breast")
         expect(page).to have_content("Pepper")
       end
@@ -43,6 +43,42 @@ RSpec.describe "Dish show page" do
       
       expect(page).to have_content("Jan's Chicken") 
       expect(page).to have_content("Calories: 125") 
+    end
+
+    it "shows a form to add an existing ingredient to that dish" do
+      jan = Chef.create!(name: "Jan's Chicken")
+      dish_1 = Dish.create!(name: "Jan's Chicken", description: "Very bland chicken, slightly overcooked.", chef: jan)
+      ing_1 = Ingredient.create!(name: "Chicken Breast", calories: 120)
+      ing_2 = Ingredient.create!(name: "Pepper", calories: 5)
+
+      dish_1.ingredients << [ing_1, ing_2]
+
+      visit dish_path(dish_1)
+      
+      expect(page).to have_content("Add Existing Ingredient To Dish") 
+    end
+
+    it "Returns you to the dish show page after adding an ingredient to the form and clicking submit" do
+      jan = Chef.create!(name: "Jan's Chicken")
+      dish_1 = Dish.create!(name: "Jan's Chicken", description: "Very bland chicken, slightly overcooked.", chef: jan)
+      ing_1 = Ingredient.create!(name: "Chicken Breast", calories: 120, id: 1)
+      ing_2 = Ingredient.create!(name: "Pepper", calories: 5, id: 2)
+      ing_3 = Ingredient.create!(name: "Salt", calories: 5, id: 3)
+
+      dish_1.ingredients << [ing_1, ing_2]
+
+      visit dish_path(dish_1)
+
+      expect(page).to have_content("Add Existing Ingredient To Dish") 
+
+      select "Salt", from: "Ingredient"
+      click_button "Submit"
+
+      expect(current_path).to eq(dish_path(dish_1))
+      
+      within ("#dish_ingredient_list") do
+        expect(page).to have_content("Salt")
+      end
     end
   end
 end
