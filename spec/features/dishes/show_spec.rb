212f448e7d17b6f8c4811dfb58017be4c24ec1c6
Jan Lehigh <janlehigh461437@gmail.com>
2024-06-04 14:48:26 -0600

    Refactor rest file for show_spec .

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -1,40 +1,46 @@
 require 'rails_helper'
 
-RSpec.describe "admin merchants index" do
+RSpec.describe "Dish show page" do
 
   describe "As a visitor when I visit the dish's show page" do
     
     it "shows the dish's name and description" do
-      dish_1 = Dish.create!(name: "Jan's Chicken", description: "Very bland chicken, slightly overcooked.")
+      jan = Chef.create!(name: "Jan's Chicken")
+      dish_1 = Dish.create!(name: "Jan's Chicken", description: "Very bland chicken, slightly overcooked.", chef: jan)
 
-      visit dishes_path(dish_1)
+      visit dish_path(dish_1)
 
       expect(page).to have_content("Jan's Chicken")
       expect(page).to have_content("Very bland chicken, slightly overcooked.")
     end
 
     it "shows a list of ingredients for that dish" do
-      dish_1 = Dish.create!(name: "Jan's Chicken", description: "Very bland chicken, slightly overcooked.")
-      ing_1 = dish_1.ingredient.create!(name: "Chicken Breast", calories: 120)
-      ing_2 = dish_1.ingredient.create!(name: "Pepper", calories: 5)
+      jan = Chef.create!(name: "Jan's Chicken")
+      dish_1 = Dish.create!(name: "Jan's Chicken", description: "Very bland chicken, slightly overcooked.", chef: jan)
+      ing_1 = Ingredient.create!(name: "Chicken Breast", calories: 120)
+      ing_2 = Ingredient.create!(name: "Pepper", calories: 5)
+      
+      dish_1.ingredients << [ing_1, ing_2]
 
-      visit dishes_path(dish_1)
+      visit dish_path(dish_1)
 
-      within ('#dish_ingredient_list') do
+      within('#dish_ingredient_list') do
         expect(page).to have_content("Chicken Breast")
         expect(page).to have_content("Pepper")
       end
     end
 
     it "shows a total calorie count for that dish and the chef's name" do
-      jan = Chef.create!(name: "Jan's Chicken", description: "Very bland chicken, slightly overcooked.")
-      dish_1 = jan.dish.create!(name: "Jan's Chicken", description: "Very bland chicken, slightly overcooked.")
+      jan = Chef.create!(name: "Jan's Chicken")
+      dish_1 = Dish.create!(name: "Jan's Chicken", description: "Very bland chicken, slightly overcooked.", chef: jan)
 
-      visit dishes_path(dish_1)
+      visit dish_path(dish_1)
       
-      expect(page).to have_content("Jan")
+      expect(page).to have_content("Jan's Chicken") 
+      expect(page).to have_content("Jan's Chicken") 
     end
   end
-
-
 end
+
+
+
