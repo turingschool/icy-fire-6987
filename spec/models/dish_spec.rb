4059915f13fc2b08760d9344e01b078bc390b4d8
Garrett Bowman <garrettbowman248@gmail.com>
2024-06-04 12:36:00 -0500

    Feat 1 Model Method

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -13,4 +13,19 @@ RSpec.describe Dish, type: :model do
       it {should have_many(:ingredients).through(:dish_ingredients)}
   end
 
+  describe "instance methods" do
+    describe "#total_calories" do
+      it "can calculate the total calories for a dish" do
+        chef = Chef.create!(name: "Gordon Ramsay")
+        dish = Dish.create!(name: "Grilled Cheese", description: "A Sandwhich as old as time, so simple yet so delicious", chef_id: chef.id)
+        ingredient_1 = Ingredient.create!(name: "Cheese", calories: 100)
+        ingredient_2 = Ingredient.create!(name: "Bread", calories: 200)
+        DishIngredient.create!(dish_id: dish.id, ingredient_id: ingredient_1.id)
+        DishIngredient.create!(dish_id: dish.id, ingredient_id: ingredient_2.id)
+
+        expect(dish.total_calories).to eq(300)
+      end
+    end
+  end
+
 end
