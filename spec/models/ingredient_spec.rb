95f8d27ef6d330a337d466ee8292bfbdfcfcfb1d
Jan Lehigh <janlehigh461437@gmail.com>
2024-06-04 14:06:12 -0600

    Refactor for dish_spec and ingredient_spec for many to many associations.

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Dish, type: :model do
 
   describe "relationships" do
       it {should have_many :dish_ingredients}
-      it {should have_many :ingredients, through: :dish_ingredients}
+      it {should have_many :dishes, through: :dish_ingredients}
   end
 
 end
