b69666169e16646181e3b5172e07f1de62abd3b2
Garrett Bowman <garrettbowman248@gmail.com>
2024-06-04 12:39:15 -0500

    Feat 2 Test

--- spec/features/dish/show_spec.rb
+++ spec/features/dish/show_spec.rb
@@ -2,12 +2,6 @@ require "rails_helper"
 
 RSpec.describe "Dish Show Page" do
     describe "As A Visitor" do
-    # As a visitor
-    # When I visit a dish's show page
-    # I see the dish’s name and description
-    # And I see a list of ingredients for that dish
-    # and a total calorie count for that dish
-    # And I see the chef's name.
         scenario "I see the dish’s name and description, a list of ingredients for that dish, a total calorie count for that dish, and the chef's name" do
             chef = Chef.create!(name: "Gordon Ramsay")
             dish = Dish.create!(name: "Grilled Cheese", description: "A Sandwhich as old as time, so simple yet so delicious", chef_id: chef.id)
@@ -30,5 +24,35 @@ RSpec.describe "Dish Show Page" do
             expect(page).to have_content(ingredient_2.name)
             end
         end
+        # As a visitor
+        # When I visit a dish's show page
+        # I see a form to add an existing Ingredient to that Dish
+        # When I fill in the form with the ID of an Ingredient that exists in the database
+        # And I click Submit
+        # Then I am redirected to that dish's show page
+        # And I see that ingredient is now listed. 
+            scenario "I see a form to add an existing Ingredient to that Dish, fill in the form with the ID of an Ingredient that exists in the database, click Submit, and see that ingredient is now listed" do
+                chef = Chef.create!(name: "Gordon Ramsay")
+                dish = Dish.create!(name: "Grilled Cheese", description: "A Sandwhich as old as time, so simple yet so delicious", chef_id: chef.id)
+                ingredient_1 = Ingredient.create!(name: "Cheese", calories: 100)
+                ingredient_2 = Ingredient.create!(name: "Bread", calories: 200)
+                ingredient_3 = Ingredient.create!(name: "Tomato", calories: 50)
+                DishIngredient.create!(dish_id: dish.id, ingredient_id: ingredient_1.id)
+
+                visit "/dishes/#{dish.id}"
+
+                within("#add_ingredient") do
+                    fill_in :ingredient_id, with: ingredient_3.id
+                    click_on "Add Ingredient"
+                end
+
+                expect(current_path).to eq("/dishes/#{dish.id}")
+
+                within("#ingredients") do
+                    expect(page).to have_content(ingredient_1.name)
+                    expect(page).to have_content(ingredient_2.name)
+                    expect(page).to have_content(ingredient_3.name)
+                end
+            end
     end
 end
