5ecf44f50419cb919f1bac2ba5e03d1209da0e81
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-06-04 09:51:30 -0600

    write more it blocks

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -11,5 +11,21 @@ RSpec.describe 'dish show page' do
       expect(page).to have_content("Omlette")
       expect(page).to have_content("Greek style")
     end
+
+    it "I see a list of ingredients for that dish" do
+      chef_1 = Chef.create!(name: "Jaque")
+      dish_1 = chef_1.dishes.create!(name: "Omlette", description: "Greek style")
+      ingredient_1 = Ingredient.create!(name: "egg", calories: 60)
+      ingredient_2 = Ingredient.create!(name: "cheese", calories: 40)
+      ingredient_3 = Ingredient.create!(name: "ice cream", calories: 100)
+      DishIngredient.create!(dish: dish_1, ingredient: ingredient_1)
+      DishIngredient.create!(dish: dish_1, ingredient: ingredient_2)
+
+      visit dish_path(dish_1)
+      
+      expect(page).to have_content(ingredient_1.name) 
+      expect(page).to have_content(ingredient_2.name)
+      expect(page).to_not have_content(ingredient_3.name)
+    end
   end
 end
