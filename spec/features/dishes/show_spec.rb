28ef9dc968f2cb0d6cc3eb521d71f55f04ba6142
Austin K <austink@Austins-MacBook-Pro.local>
2024-06-04 16:09:26 -0400

    Tests US3

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -3,12 +3,14 @@ require "rails_helper"
 RSpec.describe "Dish Show Page" do
     before :each do
         @chef = Chef.create!(name: "James May")
-        @dish = @chef.dishes.create!(name: "Spaghetti", description: "Pasta with a touch of ground beef and red sauce")
+        @dish1 = @chef.dishes.create!(name: "Spaghetti", description: "Pasta with a touch of ground beef and red sauce")
+        @dish2 = @chef.dishes.create!(name: "Meatballs", description: "Our famous ground beef with parmesan cheese")
         @ingredient1 = Ingredient.create!(name: "Pasta", calories: 200)
         @ingredient2 = Ingredient.create!(name: "Ground Beef", calories: 300)
         @ingredient3 = Ingredient.create!(name: "Tomato Sauce", calories: 100)
         @ingredient4 = Ingredient.create!(name: "Parmesan Cheese", calories: 150)
-        @dish.ingredients << [@ingredient1, @ingredient2, @ingredient3]
+        @dish1.ingredients << [@ingredient1, @ingredient2, @ingredient3]
+        @dish2.ingredients << [@ingredient2, @ingredient4]
     end
 
     it "should show the dish with the description" do
@@ -52,6 +54,24 @@ RSpec.describe "Dish Show Page" do
         click_button "Add Ingredient"
 
         expect(current_path).to eq(dish_path(@dish))
+        expect(page).to have_content(@ingredient4.name)
+    end
+
+    it "should show a link to a list of all ingredients" do
+        visit chef_path(@chef)
+
+        expect(page).to have_link("See all Ingredients")
+    end
+
+    it "should show the chef's ingredients" do
+        visit chef_path(@chef)
+
+        click_link "See all Ingredients"
+
+        expect(current_path).to eq(chef_ingredients_path(@chef))
+        expect(page).to have_content(@ingredient1.name)
+        expect(page).to have_content(@ingredient2.name)
+        expect(page).to have_content(@ingredient3.name)
         expect(page).to have_content(@ingredient4.name)
     end
 end
